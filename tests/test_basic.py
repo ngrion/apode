65263943fada41423ffcd7111a792d5b49a22fd7
Sofia Sappia <sofisappia@gmail.com>
2020-10-29 01:20:03 +0100

    Adds tests for basic module

---
+++ tests/test_basic.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of the
+#   Apode Project (https://github.com/mchalela/apode).
+# Copyright (c) 2020, Néstor Grión and Sofía Sappia
+# License: MIT
+#   Full Text: https://github.com/ngrion/apode/blob/master/LICENSE.txt
+
+import pytest
+import pandas as pd
+
+from apode.poverty import PovertyMeasures
+from apode.inequality import InequalityMeasures
+from apode.concentration import ConcentrationMeasures
+from apode.polarization import PolarizationMeasures
+
+
+def test_df_converter(uniform_ad):
+    data = uniform_ad(seed=42, size=300)
+    assert isinstance(data.data, pd.DataFrame)
+
+
+def test_call_poverty(uniform_ad):
+    data = uniform_ad(seed=42, size=300)
+    pline = 0
+    pov = PovertyMeasures(data)
+    assert pov.idf.data.equals(data.data)
+    assert pov.headcount(pline) == data.poverty.headcount(pline)
+
+
+def test_call_inequality(uniform_ad):
+    data = uniform_ad(seed=42, size=300)
+    ineq = InequalityMeasures(data)
+    assert ineq.idf.data.equals(data.data)
+    assert ineq.gini() == data.inequality.gini()
+
+def test_call_polarization(uniform_ad):
+    data = uniform_ad(seed=42, size=300)
+    pol = PolarizationMeasures(data)
+    assert pol.idf.data.equals(data.data)
+    assert pol.wolfson() == data.polarization.wolfson()
+
+def test_call_concentration(uniform_ad):
+    data = uniform_ad(seed=42, size=300)
+    conc = ConcentrationMeasures(data)
+    assert conc.idf.data.equals(data.data)
+    assert conc.rosenbluth() == data.concentration.rosenbluth()
