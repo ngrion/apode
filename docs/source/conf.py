826e815dbdca61092d95e982c252f8525a2bcdca
ngrion <72174897+ngrion@users.noreply.github.com>
2020-11-05 23:14:38 -0300

    sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,51 +12,73 @@
 #
 import os
 import sys
-# sys.path.insert(0, os.path.abspath('.'))
-sys.path.insert(0, os.path.abspath('../..'))
+import pathlib
+from setup import VERSION
+# import apode
+
+# this path is pointing to project/docs/source
+CURRENT_PATH = pathlib.Path(os.path.abspath(os.path.dirname(__file__)))
+APODE_PATH = CURRENT_PATH.parent.parent
+
+sys.path.insert(0, str(APODE_PATH))
 
 
 # -- Project information -----------------------------------------------------
 
 project = 'Apode'
-copyright = '2020, Néstor Grión and Sofía Sappia'
-author = 'Néstor Grión and Sofía Sappia'
+copyright = '2020, NÃ©stor GriÃ³n and SofÃ­a Sappia'
+author = 'NÃ©stor GriÃ³n and SofÃ­a Sappia'
 
+# The short X.Y version.
+version = VERSION
 # The full version, including alpha/beta/rc tags
-release = '0.0.1'
+release = VERSION
 
 
 # -- General configuration ---------------------------------------------------
-
-master_doc = 'index'
 
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
+    'sphinx.ext.coverage',
+    'sphinx.ext.mathjax',
+    'sphinx.ext.napoleon',
+
+    'sphinx.ext.intersphinx',
     'sphinx.ext.viewcode',
-    'sphinxcontrib.napoleon'
+    'sphinx.ext.autosummary',
+
+    'nbsphinx'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+source_suffix = ['.rst', '.md']
+
+# The master toctree document.
+master_doc = 'index'
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
+exclude_patterns = ['**.ipynb_checkpoints']
 
 # -- Options for HTML output -------------------------------------------------
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
 html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+# -- Options for nbsphinx output -------------------------------------------
+nbsphinx_prompt_width = "0pt"
---
+++ docs/source/modules.rst
@@ -0,0 +1,7 @@
+apode
+=====
+
+.. toctree::
+   :maxdepth: 4
+
+   apode
