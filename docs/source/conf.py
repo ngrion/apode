76ab969b18c5b71ae3b9fb2f5c31ce5ef9660c5c
ngrion <72174897+ngrion@users.noreply.github.com>
2020-11-10 02:03:45 -0300

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -42,14 +42,12 @@ release = '0.0.1'
 extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
-    'sphinx.ext.autosummary'
     'sphinx.ext.viewcode',
     'sphinx.ext.coverage',
     'nbsphinx',
     'sphinx.ext.mathjax',
 ]
 
-autosummary_generate = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
