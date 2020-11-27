22787023d16207fc8d88d2016e116faabbaca27e
Sofia Sappia <sofisappia@gmail.com>
2020-11-27 11:01:28 +0100

    Renames ApodeData instances as ad

--- tests/test_concentration.py
+++ tests/test_concentration.py
@@ -24,15 +24,15 @@ import pytest
 
 def test_default_call():
     ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
-    call_result = data.concentration("herfindahl")
-    method_result = data.concentration.herfindahl()
+    call_result = ad.concentration("herfindahl")
+    method_result = ad.concentration.herfindahl()
     assert call_result == method_result
 
 
 def test_invalid():
     ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
     with pytest.raises(AttributeError):
-        data.concentration("foo")
+        ad.concentration("foo")
 
 
 # =============================================================================
@@ -41,29 +41,29 @@ def test_invalid():
 def test_herfindahl_method():
     ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
     assert (
-        data.concentration.herfindahl(normalized=True) == 0.0011776319218515382
+        ad.concentration.herfindahl(normalized=True) == 0.0011776319218515382
     )
     assert (
-        data.concentration.herfindahl(normalized=False) == 0.004507039815445367
+        ad.concentration.herfindahl(normalized=False) == 0.004507039815445367
     )
 
 
 def test_herfindahl_call():
     ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
     assert (
-        data.concentration("herfindahl", normalized=True)
+        ad.concentration("herfindahl", normalized=True)
         == 0.0011776319218515382
     )
     assert (
-        data.concentration("herfindahl", normalized=False)
+        ad.concentration("herfindahl", normalized=False)
         == 0.004507039815445367
     )
 
 
 def test_herfindahl_call_equal_method():
     ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
-    call_result = data.concentration("herfindahl")
-    method_result = data.concentration.herfindahl()
+    call_result = ad.concentration("herfindahl")
+    method_result = ad.concentration.herfindahl()
     assert call_result == method_result
 
 
@@ -71,40 +71,40 @@ def test_herfindahl_empty_array():
     y = []
     df = pd.DataFrame({"x": y})
     ad = ApodeData(df, income_column="x")
-    assert data.concentration.herfindahl() == 0
+    assert ad.concentration.herfindahl() == 0
 
 
 # =============================================================================
 # TESTS ROSENBLUTH
 # =============================================================================
 def test_rosenbluth_method():
-    data = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
+    ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
     np.testing.assert_allclose(
-        data.concentration.rosenbluth(), 0.00506836225627098
+        ad.concentration.rosenbluth(), 0.00506836225627098
     )
 
 
 def test_rosenbluth_call():
-    data = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
+    ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
     np.testing.assert_allclose(
-        data.concentration("rosenbluth"), 0.00506836225627098
+        ad.concentration("rosenbluth"), 0.00506836225627098
     )
 
 
 def test_rosenbluth_call_equal_method():
-    data = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
-    call_result = data.concentration("rosenbluth")
-    method_result = data.concentration.rosenbluth()
+    ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
+    call_result = ad.concentration("rosenbluth")
+    method_result = ad.concentration.rosenbluth()
     assert call_result == method_result
 
 
 def test_rosenbluth_symmetry():
-    data = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
-    y = data.data["x"].tolist()
+    ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
+    y = ad.data["x"].tolist()
     np.random.shuffle(y)
     df2 = pd.DataFrame({"x": y})
     dr2 = ApodeData(df2, income_column="x")
-    assert data.concentration(method="rosenbluth") == dr2.concentration(
+    assert ad.concentration(method="rosenbluth") == dr2.concentration(
         method="rosenbluth"
     )
 
@@ -113,39 +113,37 @@ def test_rosenbluth_symmetry():
 # TESTS CONCENTRATION_RATIO
 # =============================================================================
 def test_concentration_ratio_method():
-    data = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
-    assert data.concentration.concentration_ratio(k=20) == 0.12913322818634668
+    ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
+    assert ad.concentration.concentration_ratio(k=20) == 0.12913322818634668
 
 
 def test_concentration_ratio_call():
-    data = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
-    assert (
-        data.concentration("concentration_ratio", k=20) == 0.12913322818634668
-    )
+    ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
+    assert ad.concentration("concentration_ratio", k=20) == 0.12913322818634668
 
 
 def test_concentration_ratio_call_equal_method():
-    data = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
-    call_result = data.concentration("concentration_ratio", k=20)
-    method_result = data.concentration.concentration_ratio(k=20)
+    ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
+    call_result = ad.concentration("concentration_ratio", k=20)
+    method_result = ad.concentration.concentration_ratio(k=20)
     assert call_result == method_result
 
 
 def test_concentration_ratio_symmetry():
-    data = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
-    y = data.data["x"].tolist()
+    ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
+    y = ad.data["x"].tolist()
     np.random.shuffle(y)
     df2 = pd.DataFrame({"x": y})
     dr2 = ApodeData(df2, income_column="x")
-    assert data.concentration(
+    assert ad.concentration(
         method="concentration_ratio", k=20
     ) == dr2.concentration(method="concentration_ratio", k=20)
 
 
 def test_concentration_k_range():
-    data = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
-    n = len(data.data.values)
+    ad = datasets.make_uniform(seed=42, size=300, mu=1, nbin=None)
+    n = len(ad.data.values)
     with pytest.raises(ValueError):
-        data.concentration(method="concentration_ratio", k=n + 1)
+        ad.concentration(method="concentration_ratio", k=n + 1)
     with pytest.raises(ValueError):
-        data.concentration(method="concentration_ratio", k=-1)
+        ad.concentration(method="concentration_ratio", k=-1)
